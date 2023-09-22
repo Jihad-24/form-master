af66abd1b51702977f7b3766e3db9d48364ccecf
Jihad-24 <mdjihat0021@example.com>
2023-09-22 19:30:05 +0600

    added component file

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Grandpa from './components/Grandpa/Grandpa'
+// import ReusableForm from './components/ReusableForm/ReusableForm'
+// import HookForm from './components/HookForm/HookForm'
+// import RefForm from './components/RefForm/RefForm'
+// import StatefulForm from './components/StatefulForm/StatefulForm'
+// import SimpleForm from './components/SimpleForm/SimpleForm'
 
 function App() {
-  const [count, setCount] = useState(0);
+
+  // const handleSingUpSubmit = data => {
+  //   console.log('sign up data', data);
+  // }
+
+  // const handleUpdateProfile = data => {
+  //   console.log('update profile', data);
+  // }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Form Master</h1>
+
+      <Grandpa></Grandpa>
+
+      {/* <SimpleForm></SimpleForm> */}
+      {/* <StatefulForm></StatefulForm> */}
+      {/* <RefForm></RefForm> */}
+      {/* <HookForm></HookForm> */}
+      {/* <ReusableForm
+        formTitle={'Sign UP'}
+        handleSubmit={handleSingUpSubmit}>
+          <div>
+            <h2>Sing UP</h2>
+            <p>please sign up right now</p>
+          </div>
+        </ReusableForm>
+      <ReusableForm
+        formTitle={'Profile Update'}
+        handleSubmit={handleUpdateProfile}
+        submitBtnText='Update'>
+          <div>
+            <h2>Update Profile</h2>
+            <p>Always keep your profile updated</p>
+          </div>
+        </ReusableForm> */}
     </>
   )
 }
